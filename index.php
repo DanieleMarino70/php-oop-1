63531b3199e1b34be763df5e9d49e7efdc0b5353
DanieleMarino70 <danielaotitan@gmail.com>
2023-03-22 20:35:12 +0100

    add: 1 milestone

--- index.php
+++ index.php
@@ -14,24 +14,20 @@ class Movie {
         $this->desc = $_desc;
         $this->duration = $_duration; 
     }
+
+
+
+    public function getAbstract($limit = 20){
+       return substr($this->desc, 0, $limit);
+
+    }
 }
 
 
+$film_tarzan = new Movie("Tarzan", "Tarzan nasce nel selvaggio deserto africano e viene cresciuto dal dolce gorilla Kala.", "88 min");
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+var_dump($film_tarzan);
 
 
 
@@ -39,3 +35,20 @@ class Movie {
 
 
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?= $film_tarzan->title ?></h1>
+    <p><?= $film_tarzan->desc ?></p>
+    <small><?= $film_tarzan->duration ?></small>
+</body>
+</html>
