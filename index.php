866f7f5b8504ee8dd87d3a14e390298325061bec
DanieleMarino70 <danielaotitan@gmail.com>
2023-03-22 19:55:10 +0100

    add: index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php 
+
+class Movie {
+    public $title;
+    public $desc;
+    public $duration;
+
+
+
+
+
+    public function __construct($_title, $_desc, $_duration){
+        $this->title = $_title;
+        $this->desc = $_desc;
+        $this->duration = $_duration; 
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
